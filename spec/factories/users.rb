1e0d8da112a46f01cce443ab8179eaa064473a6f
Mshu72 <m.shu.720@gmail.com>
2024-06-13 11:53:53 +0900

    faker-japaneseの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname                { Faker::Internet.username(specifier: 5..8) }
     email                   { Faker::Internet.email }
-    password                { Faker::Internet.password(min_length: 8, mix_case: true, special_characters: false) }
+    password                { Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false) }
     password_confirmation   { password }
-    first_name              { Faker::Name.first_name }
-    last_name               { Faker::Name.last_name }
-    first_name_kana         { 'タロウ' }
-    last_name_kana          { 'ヤマダ' }
+    first_name              { Faker::Japanese::Name.first_name }
+    last_name               { Faker::Japanese::Name.last_name }
+    first_name_kana         { Faker::Japanese::Name.first_name.yomi }
+    last_name_kana          { Faker::Japanese::Name.last_name.yomi }
     birthday                { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
