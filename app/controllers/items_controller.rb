da0d76cdae847300fdfd80e3656e2d304f161bd7
Mshu72 <m.shu.720@gmail.com>
2024-06-15 16:33:47 +0900

    CV設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,27 @@
 class ItemsController < ApplicationController
 
-def index
-end
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :detail, :price, :image, :category_id, :status_id, :prefecture_id, :shipping_fee_id, :delivery_scheduled_id).merge(user_id: current_user.id)
+  end
+
 
 end
+
