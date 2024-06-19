157248bbcd8b8208cfeeffd9e94ee530b183f5a3
Mshu72 <m.shu.720@gmail.com>
2024-06-19 15:14:18 +0900

    コントローラ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -19,17 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    if current_user.id!= @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to :show
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,6 +43,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :detail, :price, :image, :category_id, :status_id, :prefecture_id, :shipping_fee_id, :delivery_scheduled_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
 end
 
