f723f7f3e5d8200f77cf92cadbd20ac8c1468561
Mshu72 <m.shu.720@gmail.com>
2024-06-24 22:16:14 +0900

    エラーハンドリング解消

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,31 +5,34 @@ class OrdersController < ApplicationController
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_receiver = OrderReceiver.new
     @item = Item.find(params[:item_id])
+    
   end
 
   def create
-    @order_receiver = OrderReceiver.new(order_params)
+    @order_receiver = OrderReceiver.new(order_receiver_params)
+
     if @order_receiver.valid?
       pay_item
       @order_receiver.save
       return redirect_to root_path
     else
       gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      @item = Item.find(params[:item_id])
       render 'index', status: :unprocessable_entity
     end
   end
 
   private
 
-  def order_params
-    params.require(:order_receiver).permit(:price_id, :user_id, :postal_code, :prefecture, :city, :address, :building, :phone_number).merge( token: params[:token])
+  def order_receiver_params
+    params.require(:order_receiver).permit(:postal_code, :prefecture, :city, :address, :building, :phone_number).merge( token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-    amount: order_params[:price],
-    card: order_params[:token],
+    amount: order_receiver_params[:price],
+    card: order_receiver_params[:token],
     currency: 'jpy'
     )
   end
--- app/models/order_receiver.rb
+++ app/models/order_receiver.rb
@@ -1,22 +1,19 @@
 class OrderReceiver
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :price, :user_id, :token
+  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :token
 
   with_options presence: true do
-    validates :price_id
-    validates :user_id
-    validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/, message: "は「3桁ハイフン4桁」の半角文字列で入力してください"}
     validates :prefecture
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "must be 10 or 11 digits" }
-    validates :token
-  
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください" }
+    # validates :token
   end
-  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture, numericality: {other_than: 1, message: "を入力してください"}
 
   def save
-    order = Order.create(item_id: item_id, user_id: user_id, token: token)
+    order = Order.create(item_id: item_id, token: token)
   
     Receiver.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
